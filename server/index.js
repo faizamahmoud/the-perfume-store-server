cf3378c3ac11fa1d4fa3b33d24a1a6679484e3d7
faizamahmoud <faiza.mahmoud@email.com>
2022-10-10 10:27:45 -0600

    add Perfume schema

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const cors = require("cors")
 const morgan = require("morgan")
+const mongoose = require("mongoose");
 const PORT = 4000;
 
 
---
+++ server/models/Perfume.js
@@ -0,0 +1,51 @@
+
+
+const PerfumeSchema = new mongoose.Schema({
+    "id": Number,
+    "brand": {
+        type: String,
+        require: true
+      },
+    "name": {
+        type: String,
+        require: true
+      },
+    "price": {
+        type: Number,
+        require: true
+      },
+    "description": {
+        type: String,
+        require: true
+      },
+    "size": {
+        type: String,
+        require: true
+      },
+    "image": String,
+    "occasions": {
+        type: [String],
+        require: true
+      },
+    "scent-family": {
+        type: String,
+        require: true
+      },
+    "type": {
+        type: String,
+        require: true
+      },
+    "top notes": [String],
+    "middle notes": [String],
+    "base notes": [String],
+    "units": {
+        type: Number,
+        require: true
+      }
+}, { timestamps: true });
+
+
+
+const Perfume = mongoose.model("Perfume", PerfumeSchema);
+
+module.exports = Perfume;
