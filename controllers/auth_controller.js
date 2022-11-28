3273d90bff7c01009f27bc12bd20aa095b9c9f4b
faizamahmoud <faiza.mahmoud@email.com>
2022-11-28 13:40:22 -0700

    auth refactor

--- controllers/auth_controller.js
+++ controllers/auth_controller.js
@@ -2,57 +2,59 @@
 const express = require("express");
 const router = express.Router();
 const bcrypt = require("bcrypt"); // password hashing function
-// const { createUserToken} = require("../middleware/auth");
+const { createUserToken} = require("../middleware/auth");
 // const { handleValidateOwnership, requireToken } = require("../middleware/auth");
 const { User } = require("../models");
 
 
 
-// Register - keep auth
-router.post("/register", async (req, res) => {
+router.post("/register", async (req, res, next) => {
+  //   has the password before storing the user info in the database
   try {
-    console.log(req.body)
+
     const salt = await bcrypt.genSalt(10);
     const passwordHash = await bcrypt.hash(req.body.password, salt);
+
+    const pwStore = req.body.password;
+    // we store this temporarily so the origin plain text password can be parsed by the createUserToken();
+
     req.body.password = passwordHash;
     const newUser = await User.create(req.body);
-    // res.send(newUser)
-    // res.sendStatus(status)
-    res.status(200).json({ currentUser: newUser}); 
-  } catch (error) {
-    res.status(400).json(error);
+    
+		if (newUser) {
+      req.body.password = pwStore;
+      const authenticatedUserToken = createUserToken(req, newUser);
+      res.status(201).json({
+        user: newUser,
+        isLoggedIn: true,
+        token: authenticatedUserToken,
+      });
+    } else {
+      res.status(400).json({error: "Something went wrong"})
+    }
+  } catch (err) {
+    res.status(400).json({ error : err.message });
   }
 });
 
 
-router.get('/login/:userId', async (req, res, next) => {
+
+router.post("/login", async (req, res, next) => {
   try {
-    const login = req.params.userId
-    const foundUser = await User.findOne({ userId: login });
-    console.log(foundUser)
-    
-    res.status(200).json({"success":foundUser});
-    
+    const loggingUser = req.body.username;
+    const foundUser = await User.findOne({ username: loggingUser });
+    const token = await createUserToken(req, foundUser);
+    return res.status(200).json({
+      success:true,
+      message:'Logged in successfully',
+      user: foundUser,
+      isLoggedIn: true,
+      token
+    });
   } catch (err) {
-    res.status(400).json({ error: err.message });
+    res.status(404).json({ message: "Email not found", error: err.message });
   }
-})
-
-
-// Logout - does not work
-// router.delete("/logout",  async (req, res, next) => {
-//   try {
-//     const logoutUser = req.user.username
-//      console.log(logoutUser)
-//     req.logout(req.user)
-//     res.status(200).json({
-//       message: `${logoutUser} currently logged out`,
-//       isLoggedIn: false,
-//     });
-//   } catch (err) {
-//     res.status(400).json({ error: err.message });
-//   }
-// });
+});
 
 
 
--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// dependencies
 const express = require('express')
 const app = express()
 const cors = require("cors")
@@ -6,7 +7,6 @@ const methodOverride = require('method-override');
 const parser = require('body-parser');
 const urlencodedParser = parser.urlencoded({extended : false});
 const PORT = process.env.PORT || 4000;
-
 require('dotenv').config();
 
 
@@ -17,17 +17,17 @@ const user = require('./controllers/user_controller');
 
 // middleware
 
+app.use(express.json()); 
+app.use(methodOverride('_method'));
+app.use(parser.json());
 app.use(cors()); // to prevent cors errors, open access to all originsx
 app.use(morgan("dev")); // logging for development
 
-app.use(parser .json());
-app.use(urlencodedParser) 
-app.use(express.json()); 
-app.use(express.urlencoded({extended:false}))
-app.use(methodOverride('_method'));
+// app.use(urlencodedParser) 
+// app.use(express.urlencoded({extended:false}))
 app.use('/inventory', inventory);
 app.use('/auth', auth);
 app.use('/profile', user);
 
-
+// listener
 app.listen(PORT, () => console.log(`listening on PORT ${PORT}`));
